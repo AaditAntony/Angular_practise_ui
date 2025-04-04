58bde74490a08f2ff92ab877eea85579b125224a
Aadit Antony <aaditantony1112@gmail.com>
2025-04-04 17:06:00 +0530

    this is a card based ui for practise

---
+++ src/app/beautiful-card/beautiful-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beautiful-card',
+  imports: [],
+  templateUrl: './beautiful-card.component.html',
+  styleUrl: './beautiful-card.component.css'
+})
+export class BeautifulCardComponent {
+
+}
